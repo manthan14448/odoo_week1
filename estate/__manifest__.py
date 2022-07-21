b67af2f71c71598c71e61b876395ffed4b85f36a
manthangehlot <manthangelot@gmail.com>
2022-07-22 05:15:40 +0530

    4.1 :Define the real estate properties model

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real-State',
+    'version': 'not declered',
+    'category': '14',
+    'summary': 'Track leads and close opportunities',
+    'description': 'this is real state module',
+    'depends': ['base'],
+    'application': True
+}
