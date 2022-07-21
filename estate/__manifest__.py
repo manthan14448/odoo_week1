c0c43f340d1ffc62858677cdc2ebaf05581e4778
manthangehlot <manthangelot@gmail.com>
2022-07-21 06:08:42 +0530

    updated manidest file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,4 +1,10 @@
 
 {
-'name':'real-state'
+    'name': 'real-state',
+    'version': 'not declered',
+    'category': '14',
+    'summary': 'Track leads and close opportunities',
+    'description':'this is real state module',
+    'depends':['base'],
+    'application':True
 }
