ca0057ceee6b811d81d2fec095e38f6141ae5b1e
manthangehlot <manthangelot@gmail.com>
2022-08-05 05:39:20 +0530

    9.3 Add validity and date_deadline in the estate.property.offer model.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+from email.policy import default
+import string
+from datetime import datetime, timedelta
+from tracemalloc import start
 from odoo import models, fields
 
 
@@ -12,3 +17,20 @@ class estate_property_offer(models.Model):
         'res.partner', string="Patner Name", required=True)
     property_id = fields.Many2one(
         'estate.property', string="Patner id", required=True)
+    validity = fields.Integer(string="Validity", default="7")
+    date_deadline = fields.Date(compute="compute_date_deadline")
+
+    def compute_date_deadline(self):
+        date_1 = ""
+        for rec in self:
+            start_date = str(rec.property_id.date_availability)
+            date_1 = datetime.strptime(
+                start_date.replace("-", "/"), "%Y/%m/%d")
+            if(rec.validity):
+                day = rec.validity
+                end_date = date_1 + timedelta(days=day)
+                self.date_deadline = end_date
+                break
+        # start_date = datetime.now()
+        # date_compute = datetime.datetime.strptime(start_date, "%m/%d/%y")
+        # end_date = date_compute + datetime.timedelta(days=)
